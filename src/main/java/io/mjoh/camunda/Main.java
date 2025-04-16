2d340224ce2971bc8a4a55f031df61a119e579d9
Markus Johannsen <markus.johannsen@mjoh.io>
2025-04-16 19:54:37 +0200

    add camunda 8 dependencies

--- src/main/java/io/mjoh/camunda/Main.java
+++ src/main/java/io/mjoh/camunda/Main.java
@@ -1,5 +1,12 @@
 package io.mjoh.camunda;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+@Deployment(resources = "classpath*:*.bpmn")
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
